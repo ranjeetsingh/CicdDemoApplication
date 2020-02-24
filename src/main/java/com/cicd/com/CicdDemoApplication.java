e3eaf571fc49f88a2210e905a9e70f639b295ef6
Ranjeet Singh <ranjeetsi@hcl.com>
2020-02-24 11:51:31 +0530

    first commit

---
+++ src/main/java/com/cicd/com/CicdDemoApplication.java
@@ -0,0 +1,39 @@
+package com.cicd.com;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class CicdDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdDemoApplication.class, args);
+	}
+}
+	@RestController
+	class Microservice4Controller {
+
+		@Autowired
+		RestTemplate restTemplate;
+		
+		@Bean
+		public RestTemplate getRestTemplate() {
+			return new RestTemplate();
+		}
+
+		@GetMapping(value = "/getTest")
+		public String test_method() {
+			return "Hello World Test";
+		}
+		@GetMapping(value = "/")
+		public String getTest() {
+			
+			return "Hello Test demo";
+		}
+		
+}
---
+++ src/test/java/com/cicd/com/CicdDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cicd.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
