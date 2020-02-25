1c1f425ad79fb82276d900238152c06207c20a65
Ranjeet Singh <RanjeetSi@hcl.com>
2020-02-25 12:04:02 +0530

    second commit

--- src/main/java/com/cicd/com/CicdDemoApplication.java
+++ src/main/java/com/cicd/com/CicdDemoApplication.java
@@ -3,6 +3,7 @@ package com.cicd.com;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,16 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-public class CicdDemoApplication {
+public class CicdDemoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CicdDemoApplication.class, args);
 	}
 }
-
-
 	@RestController
-	@RequestMapping("/mywebapp")
+	//@RequestMapping("/mywebapp")
 	class Microservice4Controller {
 
 		@Autowired
