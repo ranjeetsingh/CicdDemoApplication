3f7a57a6fefc677682a261a51b035e22676445d7
Ranjeet Singh <RanjeetSi@hcl.com>
2020-02-27 12:09:38 +0530

    add test case

--- src/main/java/com/cicd/com/Microservice4Controller.java
+++ src/main/java/com/cicd/com/Microservice4Controller.java
@@ -8,12 +8,12 @@ public class Microservice4Controller {
 
 	@GetMapping(value = "/getTest")
 	public String test_method() {
-		return "Hello World Test-Jenkins--test-pipeline";
+		return "Hello World Test-Jenkins--test-pipeline==";
 	}
 
 	@GetMapping(value = "/")
 	public String getTest() {
 
-		return "Hello Test demo - Jenkins110-420-pipeline";
+		return "Hello Test demo - Jenkins110-420-pipeline==";
 	}
 }
