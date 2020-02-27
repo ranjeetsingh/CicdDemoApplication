4723fa8b1ad2967d0911d20b64aad4306f973c9b
Ranjeet Singh <RanjeetSi@hcl.com>
2020-02-27 10:39:02 +0530

    change message

---
+++ src/test/java/com/cicd/com/java/com/cicd/com/CicdDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cicd.com.java.com.cicd.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
