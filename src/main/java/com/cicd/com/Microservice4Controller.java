a22480798f12137e045d854e7d4c174495f1eafe
Ranjeet Singh <RanjeetSi@hcl.com>
2020-02-27 11:41:21 +0530

    test and deploy application

--- src/main/java/com/cicd/com/Microservice4Controller.java
+++ src/main/java/com/cicd/com/Microservice4Controller.java
@@ -8,12 +8,12 @@ public class Microservice4Controller {
 
 	@GetMapping(value = "/getTest")
 	public String test_method() {
-		return "Hello World Test-Jenkins";
+		return "Hello World Test-Jenkins--test pipeplne";
 	}
 
 	@GetMapping(value = "/")
 	public String getTest() {
 
-		return "Hello Test demo - Jenkins110-420";
+		return "Hello Test demo - Jenkins110-420-pipeline";
 	}
 }
