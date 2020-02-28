7ba24766fb1efd36d4783ef8533050eb26b8eb66
Ranjeet Singh <RanjeetSi@hcl.com>
2020-02-28 17:26:35 +0530

    bugfix

--- src/test/java/com/cicd/com/java/com/cicd/com/CicdDemoApplicationTests.java
+++ src/test/java/com/cicd/com/java/com/cicd/com/CicdDemoApplicationTests.java
@@ -6,7 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class CicdDemoApplicationTests {
 
-	/*
-	 * @Test void contextLoads() { }
-	 */
+	@Test
+	void contextLoads() {
+	}
+
 }
--- src/test/java/com/cicd/com/java/com/cicd/com/Microservice4ControllerTest.java
+++ src/test/java/com/cicd/com/java/com/cicd/com/Microservice4ControllerTest.java
@@ -13,21 +13,22 @@ import com.cicd.com.Microservice4Controller;
 
 @SpringBootTest
 class Microservice4ControllerTest {
-	/*
-	 * @InjectMocks Microservice4Controller microservice4Controller;
-	 * 
-	 * @Test public void test_method_When_Success() {
-	 * 
-	 * String str = microservice4Controller.test_method();
-	 * assertThat(str).isEqualTo("Hello World Test-Jenkins--test-pipeline-test100");
-	 * 
-	 * }
-	 * 
-	 * @Test public void test_getTest_When_Success() {
-	 * 
-	 * String str = microservice4Controller.getTest();
-	 * assertThat(str).isEqualTo("Hello Test demo - Jenkins110-420-pipeline");
-	 * 
-	 * }
-	 */
+	@InjectMocks
+	Microservice4Controller microservice4Controller;
+
+	@Test
+	public void test_method_When_Success() {
+
+		String str = microservice4Controller.test_method();
+		assertThat(str).isEqualTo("Hello World Test-Jenkins--test-pipeline-test100");
+
+	}
+
+	@Test
+	public void test_getTest_When_Success() {
+
+		String str = microservice4Controller.getTest();
+		assertThat(str).isEqualTo("Hello Test demo - Jenkins110-420-pipeline");
+
+	}
 }
